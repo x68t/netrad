93edf1837f3305743f6918a77ad13f20194ba16f
Hiroyuki Yamashita <hiroya@magical-technology.com>
2010-08-10 14:53:28 +0900

    Copyrightの年を変更

--- client.c
+++ client.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2008
+ * Copyright (c) 2008-2010
  *      Hiroyuki Yamashita <hiroya@magical-technology.com>.  All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
